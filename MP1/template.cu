eeba64f023bfdd7f41f539d57fb1165433615779
Tianyi Peng <tianyipeng95@gmail.com>
2024-02-24 12:05:10 -0800

    add input length

--- MP1/template.cu
+++ MP1/template.cu
@@ -1,5 +1,8 @@
 // MP 1
 #include <wb.h>
+#include <stdlib.h> // For malloc and rand
+#include <time.h>   // For time
+
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   //@@ Insert code to implement vector addition here
@@ -8,6 +11,8 @@ __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
     out[i] = in1[i] + in2[i];
   }
 }
+
+
 
 int main(int argc, char **argv) {
   wbArg_t args;
@@ -19,13 +24,38 @@ int main(int argc, char **argv) {
   float *deviceInput2;
   float *deviceOutput;
 
-  args = wbArg_read(argc, argv);
+  // Check if at least one argument was provided
+  if (argc < 2) {
+    printf("Usage: %s length\n", argv[0]);
+    return 1;
+  }
+
+  // The first positional argument is at argv[1]
+  int inputLength = atoi(argv[1]); // Convert the string argument to an integer
+
+  printf("Input Length: %d\n", inputLength);
 
   wbTime_start(Generic, "Importing data and creating memory on host");
-  hostInput1 =
-      (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
-  hostInput2 =
-      (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
+
+  // random input 
+  hostInput1 = (float *)malloc(inputLength * sizeof(float));
+  hostInput2 = (float *)malloc(inputLength * sizeof(float));
+
+   // Initialize random seed
+   srand(time(NULL));
+
+   // Fill the arrays with random values
+   for(int i = 0; i < inputLength; i++) {
+       hostInput1[i] = (float)rand() / RAND_MAX; // Random float between 0.0 and 1.0
+       hostInput2[i] = (float)rand() / RAND_MAX; // Random float between 0.0 and 1.0
+   }
+
+
+  // hostInput1 =
+  //     (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
+  // hostInput2 =
+  //     (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
+
   hostOutput = (float *)malloc(inputLength * sizeof(float));
   wbTime_stop(Generic, "Importing data and creating memory on host");
 
@@ -76,11 +106,20 @@ int main(int argc, char **argv) {
 
   wbTime_stop(GPU, "Freeing GPU Memory");
 
-  wbSolution(args, hostOutput, inputLength);
+  //wbSolution(args, hostOutput, inputLength);
 
   free(hostInput1);
   free(hostInput2);
   free(hostOutput);
+
+  // check whether the result is correct
+  for(int i = 0; i < inputLength; i++) {
+    if (hostOutput[i] != hostInput1[i] + hostInput2[i]) {
+      printf("Error: %f + %f != %f\n", hostInput1[i], hostInput2[i], hostOutput[i]);
+      return -1;
+    }
+  }
+  printf("All results are correct!\n");
 
   return 0;
 }
