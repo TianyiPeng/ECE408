adad0e78ffbddb0e206901b42fd3217aebd20e33
a3616001 <zzhong@princeton.edu>
2024-02-24 15:11:26 -0500

    update template and kxuan's code

---
+++ MP1/ruoxuan/membench.cu
@@ -0,0 +1,44 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdint.h>
+
+const int BLK = 1024;
+uint64_t n = 1024L * 1024 * 1024;
+int tt = 100;
+
+__global__ void scalamul(int n, int *s, int *a)
+{
+    int idx = threadIdx.x + blockIdx.x * BLK;
+    if (idx < n)
+        s[idx] = 2;
+    // for () {
+    // }
+}
+
+int main()
+{
+    int *a, *s;
+    cudaMalloc(&a, n * sizeof(int));
+    cudaMalloc(&s, n * sizeof(int));
+
+    struct timeval start, end;
+    float kernel_time;
+
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < tt; ++i) {
+        scalamul<<<n/BLK, BLK>>>(n, s, a);
+    }
+    cudaDeviceSynchronize();
+
+    gettimeofday(&end, NULL);
+
+    kernel_time = end.tv_sec - start.tv_sec
+        + (float) (end.tv_usec - start.tv_usec) / 1e6;
+
+    printf("time = %.2f\n", kernel_time);
+    printf("bandwidth = %.2f\n", n * sizeof(int) * tt / kernel_time / 1e9);
+
+
+    return 0;
+}
